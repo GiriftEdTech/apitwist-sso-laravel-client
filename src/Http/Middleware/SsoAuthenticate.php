2b45b3bc0fdccc489811a38e83f8e2a50bdd373e
Yasin BARAN <brnysn@gmail.com>
2024-04-30 23:46:37 +0300

    Update SsoAuthenticate.php

--- src/Http/Middleware/SsoAuthenticate.php
+++ src/Http/Middleware/SsoAuthenticate.php
@@ -39,6 +39,8 @@ class SsoAuthenticate
 
         info('session: '.json_encode(session()->all()));
 
+        info('Cookie: ' . $request->cookie('laravel_token'));
+        
         if (! $user || ! $user->ssoToken()->exists()) {
             info('no token found');
 
